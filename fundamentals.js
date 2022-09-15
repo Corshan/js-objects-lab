9fb06a73c66e2e63969b3660011debdc774b468d
Corey Shanahan <coreyshanahan01@gmail.com>
2022-09-15 12:54:07 +0100

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -12,6 +12,10 @@ const aCar = {
     }
 };
 
+aCar.milage = 10000
+aCar.colour = {exterior : "Red", interior : {texture: "leather", shade : "cream"}};
+
 // console.log(aCar.owner);
 console.log(aCar.owner + ' drives a ' + aCar.type.make);
 console.log("Reg. = " + aCar.registration.year + "-" +aCar.registration.country + "-" + aCar.registration.num);
+console.log("It is a " + aCar.colour.exterior + ", " + aCar.milage + " mileage, with " + aCar.colour.interior.texture + " interior");
