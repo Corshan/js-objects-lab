25b1c089fcdffe7e669e3ce6e55bec2360535ab6
Corey Shanahan <coreyshanahan01@gmail.com>
2022-09-15 14:25:50 +0100

    Loop constructs

--- fundamentals.js
+++ fundamentals.js
@@ -10,6 +10,7 @@ const aCar = {
         model : "Corolla",
         cc : "1.8"
     },
+    features : ['Parking Assist', 'Alarm', 'Tow-Bar'],
     registration : {
         year : "201",
         country : "WD",
@@ -21,7 +22,19 @@ aCar.milage = 10000
 aCar.colour = {exterior : "Red", interior : {texture: "leather", shade : "cream"}};
 
 // console.log(aCar.owner);
-console.log(aCar.owner + ' drives a ' + aCar.type.make);
-console.log("Reg. = " + aCar.registration.year + "-" +aCar.registration.country + "-" + aCar.registration.num);
-console.log("It is a " + aCar.colour.exterior + ", " + aCar.milage + " mileage, with " + aCar.colour.interior.texture + " interior");
-console.log('First owner : ' + aCar.previous_owners[0].name + " - " + aCar.previous_owners[0].address);
+// console.log(aCar.owner + ' drives a ' + aCar.type.make);
+// console.log("Reg. = " + aCar.registration.year + "-" +aCar.registration.country + "-" + aCar.registration.num);
+// console.log("It is a " + aCar.colour.exterior + ", " + aCar.milage + " mileage, with " + aCar.colour.interior.texture + " interior");
+// console.log('First owner : ' + aCar.previous_owners[0].name + " - " + aCar.previous_owners[0].address);
+
+for (let i = 0; i < aCar.features.length; i++){
+    console.log(aCar.features[i]);
+}
+
+for (let i = 0; i < aCar.previous_owners.length; i++){
+    console.log(aCar.previous_owners[i]);
+}
+
+for (let p in aCar.type){
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p])
+}
